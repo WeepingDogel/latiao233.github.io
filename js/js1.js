a6ad7fcd311d720db778a3950847633baeee1aad
latiao233 <vsqhq8825790@163.com>
2019-08-17 21:23:26 +0800

    修改：     index.html

---
+++ js/js1.js
@@ -0,0 +1,11 @@
+function 打开概览() {
+    document.getElementById("概览").style.display='block';
+    document.getElementById("body背景").style.backgroundImage="url(image/bj2.jpg)";
+}
+
+
+function 窗口1开(){
+    document.getElementById("窗口1").style.display='block';
+    document.getElementById("概览").style.display='none';
+    document.getElementById("body背景").style.backgroundImage="url(image/bj.jpg)";
+}
