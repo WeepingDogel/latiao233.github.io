b566c6ffa65692ce5396fe1ecb676ae3082487c5
latiao233 <vsqhq8825790@163.com>
2019-09-03 19:34:43 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 function 点击回主页(){
     window.location="index.html"
 }
+
+window.onscroll = function  cs(){
+    var 进度 = document.documentElement.scrollTop || document.body.scrollTop;
+    while(进度>200){
+        var qqq = document.getElementById("xxx")
+        document.getElementById("xxx").innerHTML="qqq";
+    }
+}
