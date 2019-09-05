1602b46319cf2af0dc744a91276daf7179148116
latiao233 <vsqhq8825790@163.com>
2019-09-05 16:21:18 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交  说明将会终止提交。  位于分支 master

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
-function 点击回主页(){
-    window.location="index.html"
+function 主页() {
+    window.location = "../index.html"
 }
 
-window.onscroll = function  cs(){
-    var 进度 = document.documentElement.scrollTop || document.body.scrollTop;
-    while(进度>200){
-        var qqq = document.getElementById("xxx")
-        document.getElementById("xxx").innerHTML="qqq";
+function 滚动进度(){
+    let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 页面总高度
+    let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 浏览器视口高度
+    let scrollAvail = pageHeight - windowHeight; // 可滚动的高度
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // 获取滚动条的高度
+    var jindu = Math.ceil(scrollTop/scrollAvail*100) //计算滚动位置并取整
+    document.getElementById("进度文本").innerHTML = jindu + '%';
+    if (jindu < 3){
+        document.getElementById("进度").style.display = 'none';
+        document.getElementById("回到顶部").style.display = 'none';
     }
+    else{
+        document.getElementById("进度").style.display = 'block';
+        document.getElementById("回到顶部").style.display = 'block';
+    }
+}
+
+function 点击回顶部(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;  
 }
